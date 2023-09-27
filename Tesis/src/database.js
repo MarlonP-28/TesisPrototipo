322d6194998d13a7dbf4e5efd0f0ee079ac5202d
MARLONP-28 <marlon.pachacama@epn.edu.ec>
2023-09-26 21:38:37 -0500

    Actual

--- Tesis/src/database.js
+++ Tesis/src/database.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/notes-db-app',{
-    useNewUrlParser: true,
+const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env
+const MONGODB_URI = 'mongodb//$(NOTES_APP_MONGODB_HOSTNOTES_APP_MONGODB_DATABAS)';
+
+mongoose.connect(MONGODB_URI,{
     useUnifiedTopology: true,
+    useNewUrlParser: true,
+    
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
-    
+
--- Tesis/src/routes/notes.js
+++
@@ -1,9 +0,0 @@
-//rutas del servidor 
-const express = require('express');
-const router = express.Router();
-
-router.get('/notes', (req, res) =>{
-    res.send('Notes from database');
-});
-
-module.exports = router;
