1a119b222b1ece937c5e46b6b9faf87c37f673af
MARLONP-28 <marlon.pachacama@epn.edu.ec>
2023-09-27 22:33:29 -0500

    MongoDB

--- Tesis/src/database.js
+++ Tesis/src/database.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 
-const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env
-const MONGODB_URI = 'mongodb//$(NOTES_APP_MONGODB_HOSTNOTES_APP_MONGODB_DATABAS)';
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 mongoose.connect(MONGODB_URI,{
     useUnifiedTopology: true,
-    useNewUrlParser: true,
-    
+    useNewUrlParser: true
 })
     .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
+    .catch(err => console.log(err));
+/*
 
+const mongoose = require('mongoose');
+const mongodbHost = process.env.notes_app_mongodb_host || '127.0.0.1:27017'; // Provide a default value
+const connectionString = `mongodb://${mongodbHost}/note-app`;
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+*/
