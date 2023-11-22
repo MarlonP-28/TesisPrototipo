e7288f31f0dbbd807998d17ed4c2493cdd597fbb
MARLONP-28 <marlon.pachacama@epn.edu.ec>
2023-11-22 11:55:00 -0500

    CambiosImportantes

--- Tesis/src/controllers/index.controller.js
+++ Tesis/src/controllers/index.controller.js
@@ -7,4 +7,8 @@ indexCtrl.renderSignin = (req, res) => {
 indexCtrl.searchArchive = (req, res) => {
   res.render("notes/search");
 };
+
+indexCtrl.renderNotes = (req, res) => {
+  res.render("notes/all-notes");
+};
 module.exports = indexCtrl;
